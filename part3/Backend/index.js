1deafd4b9a31df313a9249d0c127951e8216594b
JaroIJaro <jaro.mueller@utexas.edu>
2025-11-20 16:05:39 +0100

    server update

--- part3/Backend/index.js
+++ part3/Backend/index.js
@@ -34,6 +34,8 @@ morgan.token('body', (req) => {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
+
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
